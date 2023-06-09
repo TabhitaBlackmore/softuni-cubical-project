8a21863ee4688ac03105ee387be3f14d31fe8ac4
TabhitaBlackmore <gergana.k.ivanova.work@abv.bg>
2023-06-09 19:22:24 +0300

    add cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: '10w3gpg5bd0lior9ck2',
+        name: 'Mirror Cube',
+        description: "A cool mirror cube",
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0270/0342/0758/files/1202977061844295.QAOySrnyE0B5cIfqTxz5_height640_480x480.png?v=1663142818',
+        difficultyLevel: 4
+    },
+    {
+        id: '10w3gpg5bd0lior9ck6',
+        name: 'Magic Cube',
+        description: "A cool Rubik Cube variation",
+        imageUrl: 'https://qph.cf2.quoracdn.net/main-qimg-b587408e05f2f33526f9834a17a26a31.webp',
+        difficultyLevel: 3
+    }
+];
 
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 exports.create = (cubeData) => {
     const newCube = {
