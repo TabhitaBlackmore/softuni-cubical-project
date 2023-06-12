5fb368f2b0b31979586a0be420e5512c4fc9a47c
TabhitaBlackmore <gergana.k.ivanova.work@abv.bg>
2023-06-12 21:55:15 +0300

    add accessory to db

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,17 @@
-const router = require('express').Router(); 
+const router = require('express').Router();
+
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
     res.render('accessory/create')
 });
 
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    //TODO: add accessory data to db
+    await accessoryManager.create({ name, description, imageUrl })
+
+    res.redirect('/');
+})
 module.exports = router;
