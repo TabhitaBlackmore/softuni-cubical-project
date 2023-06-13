c8db04a40597fda4349691d0b7308b215f93930a
TabhitaBlackmore <gergana.k.ivanova.work@abv.bg>
2023-06-13 13:35:54 +0300

    Show now accessory title when none

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -39,8 +39,10 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
     const accessories = await accessoryManager.getAll().lean();
 
+    const hasAccessories = accessories.length > 0
 
-    res.render('accessory/attach', { cube, accessories });
+
+    res.render('accessory/attach', { cube, accessories, hasAccessories });
 });
 
 module.exports = router;
